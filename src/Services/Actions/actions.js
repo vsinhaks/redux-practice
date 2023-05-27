0e3915c46f0b818273802fe62fdbdefc936c5995
vsinhaks <vsinha.ks@gmail.com>
2023-05-27 22:11:05 +0530

    HomeContainer code Updated

--- src/Services/Actions/actions.js
+++ src/Services/Actions/actions.js
@@ -2,7 +2,7 @@ import {ADD_TO_CART} from '../Constants';
 
 export const addToCart =(data)=>{
     return{
-        type:'ADD_TO_CART',
+        type:ADD_TO_CART,
         data:data
     }
 }
--- src/containers/HomeContianer.js
+++ src/containers/HomeContianer.js
@@ -1,3 +1,18 @@
+import {connect} from 'react-redux';
 import Home from "../components/Home";
+import {addToCart} from'../Services/Actions/actions';
 
-export default Home;
+
+const mapStateToProps=state=>({
+
+})
+const mapDispatchToProps = dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+
+
+
+// export default Home;
